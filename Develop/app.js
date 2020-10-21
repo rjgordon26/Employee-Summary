8df12d1e90a42ad112889ac34d53083158de6911
Raymond Gordon <rjgordon26@gmail.comgit config --global user.email rjgordon26@gmail.com>
2020-10-20 22:50:00 -0500

    adding changes to JavaScript

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,6 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
